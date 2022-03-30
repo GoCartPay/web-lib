e4ac6528d7e145d0550df5268bc402d041918558
Dan Woodson <daniel.woodson@fisglobal.com>
2022-03-30 14:52:12 -0600

    building

--- dist/Theme/index.js
+++ dist/Theme/index.js
@@ -3538,7 +3538,7 @@ var mappedTheme = {
 /** Our full theme */
 var theme = createTheme(__assign(__assign({}, mappedTheme), { components: {
         MuiCssBaseline: {
-            styleOverrides: "\n      @font-face {\n        font-family: 'TWKLausanne';\n        font-style: normal;\n        font-display: swap;\n        font-weight: 400;\n        src: local('TWKLausanne'), local('Regular-TWKLausanne'), url(".concat(TWKLausanne, ") format('ttf');\n        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;\n      }\n    ")
+            styleOverrides: "\n      @font-face {\n        font-family: 'TWK Lausanne';\n        font-style: normal;\n        font-display: swap;\n        font-weight: 400;\n        src: url(".concat(TWKLausanne, ") format('truetype');\n        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;\n      }\n    ")
         }
     } }));
 console.log({ theme: theme });
