c961e29b4c4d9403e54ab7bd97086c062385df8c
Dan Woodson <daniel.woodson@fisglobal.com>
2022-03-30 14:50:19 -0600

    Corrected font setup

--- src/Theme/index.tsx
+++ src/Theme/index.tsx
@@ -22,11 +22,11 @@ export const theme = createTheme({
     MuiCssBaseline: {
       styleOverrides: `
       @font-face {
-        font-family: 'TWKLausanne';
+        font-family: 'TWK Lausanne';
         font-style: normal;
         font-display: swap;
         font-weight: 400;
-        src: local('TWKLausanne'), local('Regular-TWKLausanne'), url(${TWKLausanne}) format('ttf');
+        src: url(${TWKLausanne}) format('truetype');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
     `
