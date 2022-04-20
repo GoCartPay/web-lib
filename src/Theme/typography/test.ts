d618409f95e34298ac1d1ec36188e0f842c958cf
Simon Willems <simon.willems@fisglobal.com>
2022-04-20 09:41:57 -0600

    Remove broken typography

--- src/Theme/typography/test.ts
+++
@@ -1,6 +0,0 @@
-import { typography } from './index'
-
-// Note that if the design token changes a used value, the snapshot will need to be updated
-it('Produces correct output', () => {
-  expect(typography).toMatchSnapshot()
-})
