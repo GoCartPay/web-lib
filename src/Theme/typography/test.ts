1b9c6c1b828d75999687c6345b53e6b82ed28156
Simon Willems <simon.willems@fisglobal.com>
2022-04-28 10:14:00 -0600

    add test

--- src/Theme/typography/test.ts
+++ src/Theme/typography/test.ts
@@ -1,6 +1,6 @@
-// import { typography } from './index'
+import { typography } from './index'
 
-// // Note that if the design token changes a used value, the snapshot will need to be updated
-// it('Produces correct output', () => {
-//   expect(typography).toMatchSnapshot()
-// })
+// Note that if the design token changes a used value, the snapshot will need to be updated
+it('Produces correct output', () => {
+  expect(typography).toMatchSnapshot()
+})
