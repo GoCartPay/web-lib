46368acd5d27998b3e2cc3cc096d6a9cb6ac3942
Brian Holt <brian.holt@fisglobal.com>
2022-06-15 11:13:40 -0700

    GOCART-3863 refactoring styling

--- src/components/Otp/index.tsx
+++ src/components/Otp/index.tsx
@@ -1,74 +1,70 @@
-/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
-/** @jsxImportSource @emotion/react */
 import React, { useState } from 'react';
 import Textfield, { TextFieldProps } from '@mui/material/TextField';
 import { FormGroup, Paper } from '@mui/material';
-import { css } from '@emotion/react';
 import RemoveIcon from '@mui/icons-material/Remove';
 import Box from '@mui/system/Box';
 
-const styles = {
-  "&.MuiInput-root": {
-    fontFamily: 'TWK Lausanne!important',
-    textAlign: 'center !important'
-  },
-  // "&.MuiButton-text": {
-  //   '&:hover': {
-  //   },
-  //   '&:focus': {
-  //   },
-  // },
-  // "&.MuiButton-outlined": {
-  //   '&:hover': {
-  //   },
-  //   '&:focus': {
-  //   },
-  // },
+const otpOuterWrapperStyles = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'relative',
 };
-// -webkit-transform:scale(2.0, 1.0);
-// -moz-transform:scale(2.0, 1.0);
-// -ms-transform:scale(2.0, 1.0);
-// -o-transform:scale(2.0, 1.0);
-// transform:scale(2.0,1.0);
-const cssStyles = css`
-  display: flex;
-  justify-content: flex;
-  input {
-    text-align: center !important;
-    border-radius: 8px;
-    color: transparent;
-    font-family: TWK Lausanne !important;
-    font-size: 20px;
-    line-height: 25px;
-    letter-spacing: 8px;
-    font-weight: 500px;
+
+const textFieldSxStyles = {
+  ".MuiInputBase-input": {
+    textAlign: 'center !important',
+    borderRadius: '8px',
+    color: 'transparent',
+    fontSize: '20px',
+    lineHeight: '25px',
+    letterSpacing: '8px',
+  },
+  "& .MuiOutlinedInput-root:hover": {
+    "& > fieldset": {
+      borderColor: '#DCDEE5',
+      backgroundColor: '#F1F2F5',
+    },
+  },
+  "& .MuiOutlinedInput-root.Mui-focused": {
+    "& > fieldset": {
+      borderColor: "#117B7433",
+      backgroundColor: '#F5FAF9',
+    }
   }
-  .MuiFormGroup-root {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-`;
+};
+
+const otpInnerWrapperStyles = {
+  position: 'absolute',
+  display: 'flex',
+  height: 1,
+  width: 0.33,
+  justifyContent: 'center',
+  pointerEvents: 'none'
+};
+
+const otpDigitStyles = {
+  flex: 1,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 32,
+  mx: 0.25,
+  color: '#757575'
+};
 
 const generateOtp = (authCode: string, codeLength: number) => {
   const codeInputs = [];
 
-  for (let x = 0; x < codeLength; x++){
+  for (let x = 0; x < codeLength; x++) {
     codeInputs.push(
       // these styles are good to go
-    <Box
-      sx={{
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 32,
-        mx: 0.25
-      }}
-    >
-    {authCode[x] || <RemoveIcon sx={{fontSize: 32 }}/>}
-    </Box>
-  )}
+      <Box
+        sx={otpDigitStyles}
+      >
+        {authCode[x] || <RemoveIcon sx={{ fontSize: 32 }} />}
+      </Box>
+    )
+  }
 
   return codeInputs;
 };
@@ -80,7 +76,7 @@ type OtpProps = TextFieldProps & {
 const Otp: React.FC<OtpProps> = ({
   codeLength = 6,
   autoFocus = false,
-  ...props 
+  ...props
 }) => {
   const [authCode, setAuthCode] = useState('');
   const [focusedField, setFocusedField] = useState(0);
@@ -99,14 +95,8 @@ const Otp: React.FC<OtpProps> = ({
   };
 
   return (
-    <Paper css={cssStyles}>
-      <FormGroup css={css`
-         justify-items: center;
-         justify-content: center;
-         align-content: center;
-         position: relative;
-         flex-wrap: nowrap !important;
-      `}>
+    <Paper>
+      <FormGroup sx={otpOuterWrapperStyles}>
         <Textfield
           autoFocus={autoFocus}
           variant='outlined'
@@ -119,25 +109,9 @@ const Otp: React.FC<OtpProps> = ({
             maxLength: 6
           }}
           {...props}
-          css={css`
-          position: absolute;
-          letter-spacing: 2.0em;
-          outline: none;
-          border: none;
-          background: none;
-          box-sizing: border-box;
-          color: transparent;
-          caret-color: transparent;
-          `}
+          sx={textFieldSxStyles}
         />
-        <Box sx={{
-          position: 'absolute',
-          display: 'flex',
-          height: 1,
-          width: 0.33,
-          justifyContent: 'center',
-          pointerEvents: 'none'
-        }}>
+        <Box sx={otpInnerWrapperStyles}>
           {generateOtp(authCode, codeLength)}
         </Box>
       </FormGroup>
