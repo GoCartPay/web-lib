af695d22a39a1885c0fc3a6a473e4c68afe91d84
Simon Willems <simontwillems@gmail.com>
2022-06-23 15:58:58 -0600

    Feature/delete dist (#23)
    * Delete dist
    
    * add to gitignore
    
    Co-authored-by: Simon Willems <simon.willems@fisglobal.com>

--- dist/Theme/shadows/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @file Process the design token's `effect.elevation` settings into
- * something ready to use with MUI's `createTheme`
- **/
-import type { Shadows } from '@mui/material/styles/shadows';
-export declare const shadows: Shadows;
--- dist/util/Logger/index.js
+++
@@ -1,4 +0,0 @@
-const Div = () => console.log('++++ HIT +++++');
-
-export { Div, Div as default };
-//# sourceMappingURL=index.js.map
