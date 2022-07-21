087ff8aeb8b7383857d27e9979f8b564d70e8620
Melissa <melissa.purdy@fisglobal.com>
2022-07-21 10:41:08 -0600

    GOCART-4544 adding Purchase Details to exported components

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { BigButton } from './components/BigButton';
 import { EntryBackgroundDrawer } from './components/EntryBackgroundDrawer';
 import LegalFooter from './components/LegalFooter';
 import { Loader } from './components/Loader';
+import PurchaseDetails from './components/PurchaseDetails';
 import Otp from './components/Otp';
 import RadioGroup from './components/RadioGroup';
 import RadioButton from './components/RadioButton';
@@ -13,7 +14,8 @@ export {
     LegalFooter,
     Loader,
     Otp,
+    PurchaseDetails,
     RadioGroup,
     RadioButton,
-    SmallButton
+    SmallButton,
 }
