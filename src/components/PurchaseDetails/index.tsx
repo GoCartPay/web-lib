983aaaac3c1bd9861c34c68be8dfe64c41112680
Melissa <melissa.purdy@fisglobal.com>
2022-07-22 10:47:44 -0600

    GOCART-4544 updating logo styles

--- src/components/PurchaseDetails/index.tsx
+++ src/components/PurchaseDetails/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import { makeStyles } from '@mui/styles'
 import Typography from '@mui/material/Typography';
 
 /** Image styles */
-const useStyles = makeStyles({
-  logo: {
-    maxWidth: '148px',
-    maxHeight: '148px',
-    objectFit: 'contain',
-  },
-});
+const logoStyles = {
+  maxWidth: '148px',
+  maxHeight: '148px',
+};
 
 /** Type info */
-
 type PurchaseDetails = {
   label: string,
   description: string,
@@ -40,43 +35,39 @@ const PurchaseDetails: React.FC<PurchaseDetailsProps> = ({
   purchaseDetails = [],
   logoSrc,
   merchantName,
-}) => {
-  const styles = useStyles();
-
-  return (
-    <Box sx={{ width: '327px', height: '191px'}}>
-      <Typography variant='h6' sx={{ fontWeight: 500, mb: '12px', lineHeight: '26px' }}>
-        {title}
-      </Typography>
-      <Box sx={{ display: 'flex' }}>
-        { logoSrc && (
-          <Box sx={{ maxWidth: '148px', maxHeight: '148px', mr: '8px' }}>
-            <img className={styles.logo} src={logoSrc} alt={merchantName} />
-          </Box>
-        )}
-        <Box sx={{ maxWidth: logoSrc ? '170px' : '327px'}}>
-        { purchaseDetails?.map((purchaseDetail) => {
-          const { label, description, level } = {...purchaseDetail};
-          const displayPair = label && description;
-          const containsNewLine = description?.includes('\n')
-
-          return displayPair && (
-            <div key={`label-and-description-pair-level${level}`}>
-              <Typography key={`label-level${level}`} variant={level === 1 ? 'subtitle1' : 'subtitle2'}>
-                { label }
-              </Typography>
-              <Typography key={`description-level${level}`} variant='body2' noWrap={!containsNewLine} color='textSecondary' sx={{ mb: '6px' }}>
-                {description?.split('\n').map(str =>
-                  <span key={`${str}-line-br`}>{str} <br/> </span>
-                )}
-              </Typography>
-            </div>
-          )})
-        }
+}) => (
+  <Box sx={{ width: '327px', height: '191px'}}>
+    <Typography variant='h6' sx={{ fontWeight: 500, mb: '12px', lineHeight: '26px' }}>
+      {title}
+    </Typography>
+    <Box sx={{ display: 'flex' }}>
+      { logoSrc && (
+        <Box sx={{ maxWidth: '148px', maxHeight: '148px', mr: '8px' }}>
+          <img style={logoStyles} src={logoSrc} alt={merchantName} />
         </Box>
+      )}
+      <Box sx={{ maxWidth: logoSrc ? '170px' : '327px'}}>
+      { purchaseDetails?.map((purchaseDetail) => {
+        const { label, description, level } = {...purchaseDetail};
+        const displayPair = label && description;
+        const containsNewLine = description?.includes('\n')
+
+        return displayPair && (
+          <div key={`label-and-description-pair-level${level}`}>
+            <Typography key={`label-level${level}`} variant={level === 1 ? 'subtitle1' : 'subtitle2'}>
+              { label }
+            </Typography>
+            <Typography key={`description-level${level}`} variant='body2' noWrap={!containsNewLine} color='textSecondary' sx={{ mb: '6px' }}>
+              {description?.split('\n').map(str =>
+                <span key={`${str}-line-br`}>{str} <br/> </span>
+              )}
+            </Typography>
+          </div>
+        )})
+      }
       </Box>
     </Box>
-  );
-};
+  </Box>
+);
 
 export default PurchaseDetails;
